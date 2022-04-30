a4b7ab57a9f63b4ac1881cfd5d256590613cd7b3
Jitka Hladká <jitka.hladka@gmail.com>
2022-04-30 11:25:18 +0200

    Zobrazování a skrytí navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
 console.log('funguju!');
+
+document.querySelector('#nav-btn').addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
+
+const navItems = document.querySelectorAll('nav a');
+
+navItems.forEach((navItem) =>
+  navItem.addEventListener('click', () => {
+    document.querySelector('nav').classList.add('nav-closed');
+  }),
+);
