a2b03db4e0049e94a409a319fd0c6d941d55be37
Jitka Hladká <jitka.hladka@gmail.com>
2022-04-30 11:44:01 +0200

    Objednávací tlačítko

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ document.querySelector('#nav-btn').addEventListener('click', () => {
   document.querySelector('nav').classList.toggle('nav-closed');
 });
 
+//skrývání hamburger navigace
 const navItems = document.querySelectorAll('nav a');
 
 navItems.forEach((navItem) =>
@@ -13,3 +14,21 @@ navItems.forEach((navItem) =>
     document.querySelector('nav').classList.add('nav-closed');
   }),
 );
+
+//objednávací tlačítko
+let ordered = false;
+
+const orderBtn = document.querySelector('.order-btn');
+orderBtn.addEventListener('click', () => {
+  if (ordered === false) {
+    orderBtn.textContent = 'Zrušit';
+    document.querySelector('.drink__cup').classList.add('drink__cup--selected');
+    ordered = true;
+  } else {
+    orderBtn.textContent = 'Objednat';
+    document
+      .querySelector('.drink__cup')
+      .classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+});
